f16259fa5f9243f26b455e19e1e6441a8287f929
AnishNavalgund <107324261+AnishNavalgund@users.noreply.github.com>
2022-07-07 02:30:10 +0200

    Update TestSup.py

--- TestSup.py
+++ TestSup.py
@@ -9,60 +9,68 @@ root = Tk()
 root. title('Smart Parking System')
 
 # Adjust size
-root.geometry("1000x850")
+root.geometry("1500x850")
 root.resizable(False, False)
 
 # Add image file
-bg = PhotoImage(file = "ParkTest.png")
+bg = PhotoImage(file = "ParkTest1.png")
 
 # Create Canvas
-canvas1 = Canvas( root, width = 1000, height = 850)
+canvas1 = Canvas( root, width = 1500, height = 850)
 
 canvas1.pack(fill = "both", expand = True)
 
 # Display image
-canvas1.create_image( 10, 100, image = bg, anchor = "nw") 
+canvas1.create_image( 0, 0, image = bg, anchor = "nw") 
 
 # Add Text 
-canvas1.create_text( 500, 20, fill="Black",font="Times 30 italic bold", text = "Smart Park Capsule")
+canvas1.create_text( 800, 50, fill="Blue",font="Times 30 italic bold", text = "Smart Park Capsule")
 
-canvas1.create_text( 190, 100, fill="Black",font="Andy 20 bold", text = "Parking Spot - 1")
-canvas1.create_text( 490, 100, fill="Black",font="Andy 20 bold", text = "Parking Spot - 2")
-canvas1.create_text( 790, 100, fill="Black",font="Andy 20 bold", text = "Parking Spot - 3")
+canvas1.create_text( 200, 150, fill="Cyan",font="Andy 20 bold", text = "Parking Spot - 1")
+canvas1.create_text( 800, 150, fill="Cyan",font="Andy 20 bold", text = "Parking Spot - 2")
+canvas1.create_text( 1300, 150, fill="Cyan",font="Andy 20 bold", text = "Parking Spot - 3")
 
 
 led = tk_tools.Led(canvas1, size=100)
 # led.grid(row=5, column=0, sticky='news')
-led_canvas = canvas1.create_window(120,150,anchor="nw",window = led)
+led_canvas = canvas1.create_window(170,200,anchor="nw",window = led)
 #led.pack()
 led.to_grey()
 
 led1 = tk_tools.Led(canvas1, size=100)
 # led.grid(row=5, column=0, sticky='news')
-led_canvas = canvas1.create_window(420,150,anchor="nw",window = led1)
+led_canvas = canvas1.create_window(770,200,anchor="nw",window = led1)
 #led.pack()
 led1.to_grey()
 
 led2 = tk_tools.Led(canvas1, size=100)
 # led.grid(row=5, column=0, sticky='news')
-led_canvas = canvas1.create_window(720,150,anchor="nw",window = led2)
+led_canvas = canvas1.create_window(1250,200,anchor="nw",window = led2)
 #led.pack()
 led2.to_grey()
 
-canvas1.create_text( 480, 450, fill="Black",font="Andy 20 bold", text = "Parking Gate")
+canvas1.create_text( 400, 500, fill="Yellow",font="Andy 20 bold", text = "Parking Gate")
+
+led3 = tk_tools.Led(canvas1, size=100)
+# led.grid(row=5, column=0, sticky='news')
+led_canvas = canvas1.create_window(350,550,anchor="nw",window = led3)
+#led.pack()
+led2.to_grey()
+
+canvas1.create_text( 1100, 500, fill="Yellow",font="Andy 20 bold", text = "Temperature Monitor")
 
 def digitalclock():
    text_input = time.strftime("Time:%H:%M:%S")
    label.config(text=text_input)
    label.after(200, digitalclock)
    
-label = Label(canvas1, font=("Courier", 10, 'bold'), bg="white", fg="black", bd =10)
-DigitalCLock_Label = canvas1.create_window( 850, 15, anchor = "nw", window = label)
+label = Label(canvas1, font=("Courier", 12, 'bold'), bg="Black", fg="White", bd =10)
+DigitalCLock_Label = canvas1.create_window( 1300, 15, anchor = "nw", window = label)
 
 date = dt.datetime.now()
-format_date = f"{date:%a, %b %d %Y}"
+format_date = f"{date: %d %b %Y}"
 #display(format_date)
-canvas1.create_text( 910, 65, fill="Black",font="Times 12 bold", text = format_date)
+canvas1.create_text( 1370, 65, fill="White",font="Times 12 bold", text = format_date)
 
 #topic1 = Label(canvas1, font=("Courier", 30, 'bold'), bg="lightblue", fg="black", bd =80)
 #button1_canvas = canvas1.create_window( 50, 150, anchor = "nw",window = topic1)
