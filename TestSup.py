f13d410d77c2f9cdc764cbaadff1b0f3525f64a6
ssakpal2 <104564045+ssakpal2@users.noreply.github.com>
2022-07-07 06:26:36 +0530

    Update TestSup.py

--- TestSup.py
+++ TestSup.py
@@ -1,11 +1,12 @@
 from tkinter import *
+import tkinter as tk
 import tk_tools
 import time
 import datetime as dt
 #import random
 import json
 
-root = Tk()
+root =tk.Tk()
 root. title('Smart Parking System')
 
 # Adjust size
@@ -29,6 +30,30 @@ canvas1.create_text( 800, 50, fill="Blue",font="Times 30 italic bold", text = "S
 canvas1.create_text( 200, 150, fill="Cyan",font="Andy 20 bold", text = "Parking Spot - 1")
 canvas1.create_text( 800, 150, fill="Cyan",font="Andy 20 bold", text = "Parking Spot - 2")
 canvas1.create_text( 1300, 150, fill="Cyan",font="Andy 20 bold", text = "Parking Spot - 3")
+
+canvas1.create_text( 150, 360, fill="Cyan",font="Andy 13 bold", text = "In_Time")
+entry1 = tk.Entry (root) 
+canvas1.create_window(150, 380, window=entry1)
+
+canvas1.create_text( 300, 360, fill="Cyan",font="Andy 13 bold", text = "Out_Time")
+entry2 = tk.Entry (root) 
+canvas1.create_window(300, 380, window=entry2)
+
+canvas1.create_text( 730, 360, fill="Cyan",font="Andy 13 bold", text = "In_Time")
+entry3 = tk.Entry (root) 
+canvas1.create_window(730, 380, window=entry3)
+
+canvas1.create_text( 880, 360, fill="Cyan",font="Andy 13 bold", text = "Out_Time")
+entry4 = tk.Entry (root) 
+canvas1.create_window(880, 380, window=entry4)
+
+canvas1.create_text( 1250, 360, fill="Cyan",font="Andy 13 bold", text = "In_Time")
+entry5 = tk.Entry (root) 
+canvas1.create_window(1250, 380, window=entry5)
+
+canvas1.create_text( 1400, 360, fill="Cyan",font="Andy 13 bold", text = "Out_Time")
+entry6 = tk.Entry (root) 
+canvas1.create_window(1400, 380, window=entry6)
 
 
 led = tk_tools.Led(canvas1, size=100)
